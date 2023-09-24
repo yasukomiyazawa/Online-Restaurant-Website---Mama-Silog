22a2f43e947566eba2bf3c403735ce27d74cc606
Yasuko Miyazawa <yasukomiyazawa08@gmail.com>
2023-09-24 19:53:09 +0900

    git a hard reset clone to the previous commit of bestseller section being made to try error fix on deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
 import { BestSellers } from "@/components";
 import Image from "next/image";
+import { CustomButton } from "@/components";
 
 export default function Home() {
   return (
     // min-h-screen
     <main className="flex flex-col items-center justify-between">
       <BestSellers />
+      <div className="w-full bg-silogMeals2 bg-cover bg-center">
+        <div className="h-1/4 flex justify-around py-20 text-white font-bold">
+          <p className="text-4xl">Order Now!</p>
+
+          <CustomButton
+            title="Click Here"
+            href="/menu"
+            style="rounded-lg text-2xl"
+          />
+
+          <p className="text-4xl">See Menu</p>
+        </div>
+      </div>
+      {/* <Blogs /> */}
     </main>
   );
 }
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -5,7 +5,11 @@ import Link from "next/link";
 const CustomButton = ({ title, style, href }: CustomButtonProps) => {
   return (
     <Link href="{href}">
-      <button className={style}>{title}</button>
+      <button
+        className={`border border-yellow-400 bg-yellow-400 py-2 px-5 hover:bg-rose-900 ${style}`}
+      >
+        {title}
+      </button>
     </Link>
   );
 };
